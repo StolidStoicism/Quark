f53e602b9e1130cb7c6148a58f71275a41d84080
Andre Vallestero <andrevallestero@gmail.com>
2019-11-04 19:18:53 -0500

    Minimized

--- main.c
+++ main.c
@@ -13,55 +13,38 @@ int hook_process(char* strProcessName, PROCESS_HOOK* phData) {
 	
 	PROCESSENTRY32 ProcEntry;
 	ProcEntry.dwSize = sizeof(PROCESSENTRY32);
-
-    if (Process32First(hSnapshot, &ProcEntry)) {
+	Process32First(hSnapshot, &ProcEntry);
+	
+	do {
 		if (!strcmp(ProcEntry.szExeFile, strProcessName)) {
 			CloseHandle(hSnapshot);
 			phData->hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcEntry.th32ProcessID);
 			phData->dwProcessId = ProcEntry.th32ProcessID;
     	    return 1;
     	}
-    } else {
-		CloseHandle(hSnapshot);
-		return 0;
-    }
-
-	while (Process32Next(hSnapshot, &ProcEntry)) {
-    	if (!strcmp(ProcEntry.szExeFile, strProcessName)) {
-			phData->hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcEntry.th32ProcessID);
-			phData->dwProcessId = ProcEntry.th32ProcessID;
-    	    return 1;
-    	}
-    }
+	} while (Process32Next(hSnapshot, &ProcEntry));
 
 	CloseHandle(hSnapshot);
+	printf("Process \"%s\" not found\n", strProcessName);
 	return 0;
 }
 
 int grab_process_module(DWORD dwProcessId, char* strModuleName, MODULEENTRY32* pModuleEntry) {
 	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwProcessId);
 	if (hSnapshot == INVALID_HANDLE_VALUE) return 0;
-
+	
 	pModuleEntry->dwSize = sizeof(MODULEENTRY32);
-
-	if (Module32First(hSnapshot, pModuleEntry)) {
+	Module32First(hSnapshot, pModuleEntry);
+	do {
+		printf("Module: %s\n", pModuleEntry->szModule);
 		if (!strcmp(pModuleEntry->szModule, strModuleName)) {
 			CloseHandle(hSnapshot);
 			return 1;
 		}
-	} else {
-		CloseHandle(hSnapshot);
-		return 0;
-	}
-
-	while (Module32Next(hSnapshot, pModuleEntry)) {
-		if (!strcmp(pModuleEntry->szModule, strModuleName)) {
-			CloseHandle(hSnapshot);
-			return 1;
-		}
-	}
+	} while (Module32Next(hSnapshot, pModuleEntry));
 
 	CloseHandle(hSnapshot);
+	printf("Module \"%s\" not found, error code %lu\n", strModuleName, GetLastError());
 	return 0;
 }
 
@@ -73,5 +56,6 @@ int main(void) {
 	MODULEENTRY32 meClient;
 	if(!grab_process_module(pPhCsgo->dwProcessId, "client.dll", &meClient)) return 0;
 	
+	getchar();
 	return 0;
 }
